21c27b77ba7faaf6de1e38deb6352e02c4e36acf
kamScripts <k.gebski1@gmail.com>
2026-01-31 19:35:46 +0000

    Functional_tests: update to unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,21 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_todo_list(self):
+        #Edith has heard about a cool new online t-do app.
+        #She goes to check out its hompage
+        self.browser.get('http://localhost:8000')
 
-assert 'Congratulations!' in browser.title
-print('Ok')
+        #She notices the page title and header mention to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+
+        #She is invited to enter a to-do item staright away
+        self.fail('Complete the test')
+
+if __name__ == '__main__':
+    unittest.main()
