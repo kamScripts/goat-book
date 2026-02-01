c200f86d032a34788a6d4e347942d90e5fd85832
kamScripts <k.gebski1@gmail.com>
2026-02-01 09:32:53 +0000

    First unit test and view function

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
+class HomePageTests(TestCase):
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertIn('<title>To-Do Lists</title>', html)
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.endswith('</html>'))
 
-class SmokeTest(TestCase):
-    def test_smoke(self):
-        self.assertTrue(False)
